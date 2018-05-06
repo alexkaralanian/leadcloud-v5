45087850db3bea00d652b5da5074016e22d196fb
Alex Karalanian <alex.karalanian@gmail.com>
2018-05-06 11:54:15 -0400

    updated config setting

--- config/prod.js
+++ config/prod.js
@@ -2,9 +2,10 @@ module.exports = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   SESSION_SECRET: process.env.SESSION_SECRET,
-  DB_USERNAME: RDS_USERNAME,
-  DB_PASSWORD: process.env.RDS_PASSWORD,
+  DB_HOST: process.env.RDS_HOSTNAME,
+  DB_PORT: process.env.RDS_PORT,
   DB_NAME: process.env.RDS_DB_NAME,
-  DB_HOST: proces.env.RDS_HOSTNAME,
+  DB_USERNAME: process.env.RDS_USERNAME,
+  DB_PASSWORD: process.env.RDS_PASSWORD,
   REDIS_URI: process.env.REDIS_URI
 };
