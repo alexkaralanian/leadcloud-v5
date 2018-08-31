e4eba585a68074933629dafca11fed18da2372b1
Alex Karalanian <alex.karalanian@gmail.com>
2018-08-31 18:28:02 -0400

    listings hotfix

--- client/src/components/Listings/Listings.jsx
+++ client/src/components/Listings/Listings.jsx
@@ -11,55 +11,60 @@ const Listings = ({ listings, isFetching, isListingNew }) =>
   isFetching ? (
     <Loading />
   ) : (
-    <div className="table_container">
-      <Table striped>
-        <thead>
-          <tr>
-            <th />
-            <th>Address</th>
-            <th>Updated</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listings &&
-            listings.map(listing => (
-              <tr key={listing.id}>
-                <td>
-                  {listing && listing.images ? (
-                    <div className="table_img">
-                      <img
-                        src={listing.images[0]}
-                        alt="Listing_Image_Primary"
-                      />
-                    </div>
-                  ) : (
-                    <div className="table_img-null">
-                      <span>
-                        {listing && listing.address
-                          ? listing.address.charAt(0).toUpperCase()
-                          : null}
-                      </span>
-                    </div>
-                  )}
-                </td>
-                <td>
-                  {listing.address ? (
-                    <Link to={`/listings/${listing.id}`}>
-                      {listing.address}
-                    </Link>
-                  ) : (
-                    ""
-                  )}
-                </td>
-
-                <td>
-                  {moment(listing.updated).format("ddd, M/D/YY h:mma") || null}
-                </td>
+    <React.Fragment>
+      {listings.length > 0 && (
+        <div className="table_container">
+          <Table striped>
+            <thead>
+              <tr>
+                <th />
+                <th>Address</th>
+                <th>Updated</th>
               </tr>
-            ))}
-        </tbody>
-      </Table>
-    </div>
+            </thead>
+            <tbody>
+              {listings &&
+                listings.map(listing => (
+                  <tr key={listing.id}>
+                    <td>
+                      {listing && listing.images ? (
+                        <div className="table_img">
+                          <img
+                            src={listing.images[0]}
+                            alt="Listing_Image_Primary"
+                          />
+                        </div>
+                      ) : (
+                        <div className="table_img-null">
+                          <span>
+                            {listing && listing.address
+                              ? listing.address.charAt(0).toUpperCase()
+                              : null}
+                          </span>
+                        </div>
+                      )}
+                    </td>
+                    <td>
+                      {listing.address ? (
+                        <Link to={`/listings/${listing.id}`}>
+                          {listing.address}
+                        </Link>
+                      ) : (
+                        ""
+                      )}
+                    </td>
+
+                    <td>
+                      {moment(listing.updated).format("ddd, M/D/YY h:mma") ||
+                        null}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </div>
+      )}
+    </React.Fragment>
   );
 
 export default Listings;
