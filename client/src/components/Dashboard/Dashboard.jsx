62a14a10724f5e7c43bd3d029a8c483553d27fbc
Alex Karalanian <alex.karalanian@gmail.com>
2018-09-21 16:48:36 -0400

    added scss template files

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -2,44 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Grid, Col, Row } from "react-bootstrap";
-import "./Dashboard.css";
+// import "./Dashboard.css";
 
 const Dashboard = () => (
   <div className="background">
-    <Grid className="content">
+    <Grid className="dashboard__content margin-top-2">
       <Row>
         <Col sm={6}>
-          <Link className="navLink" to="/contacts">
-            <div className="dashCard">
-              <h3 className="dashText">Contacts</h3>
+          <Link className="dashboard__content-link" to="/contacts">
+            <div className="dashboard__content-card">
+              <h3 className="dashboard__content-text">Contacts</h3>
             </div>
           </Link>
         </Col>
         <Col sm={6}>
-          <Link className="navLink" to="/listings">
-            <div className="dashCard">
-              <h3 className="dashText">Listings</h3>
+          <Link className="dashboard__content-link" to="/listings">
+            <div className="dashboard__content-card">
+              <h3 className="dashboard__content-text">Listings</h3>
             </div>
           </Link>
         </Col>
         <Col sm={6}>
-          <Link className="navLink" to="/groups">
-            <div className="dashCard">
-              <h3 className="dashText">Groups</h3>
+          <Link className="dashboard__content-link" to="/groups">
+            <div className="dashboard__content-card">
+              <h3 className="dashboard__content-text">Groups</h3>
             </div>
           </Link>
         </Col>
         <Col sm={6}>
-          <Link className="navLink" to="/campaigns">
-            <div className="dashCard">
-              <h3 className="dashText">Campaigns</h3>
+          <Link className="dashboard__content-link" to="/campaigns">
+            <div className="dashboard__content-card">
+              <h3 className="dashboard__content-text">Campaigns</h3>
             </div>
           </Link>
         </Col>
         <Col sm={6}>
-          <Link className="navLink" to="/emails">
-            <div className="dashCard">
-              <h3 className="dashText">Messages</h3>
+          <Link className="dashboard__content-link" to="/emails">
+            <div className="dashboard__content-card">
+              <h3 className="dashboard__content-text">Messages</h3>
             </div>
           </Link>
         </Col>
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,17 @@ import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+// import "./scss/main.scss";
+
+import "flag-icon-css/css/flag-icon.min.css";
+// Import Font Awesome Icons Set
+import "font-awesome/css/font-awesome.min.css";
+// Import Simple Line Icons Set
+import "simple-line-icons/css/simple-line-icons.css";
+// Import Main styles for this application
+import "./scss/style.scss";
+// Temp fix for reactstrap
+// import "../scss/core/_dropdown-menu-right.scss";
 
 import store from "./store";
 import App from "./routes";
