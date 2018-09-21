4e674ff0dd05417eb11c272b02e83b88dbdc5a8d
Alex Karalanian <alex.karalanian@gmail.com>
2018-09-21 17:04:49 -0400

    added scss template files

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,13 @@ import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-// import "./scss/main.scss";
 
-import "flag-icon-css/css/flag-icon.min.css";
-// Import Font Awesome Icons Set
-import "font-awesome/css/font-awesome.min.css";
-// Import Simple Line Icons Set
-import "simple-line-icons/css/simple-line-icons.css";
-// Import Main styles for this application
+// import "flag-icon-css/css/flag-icon.min.css";
+
+// import "font-awesome/css/font-awesome.min.css";
+
+// import "simple-line-icons/css/simple-line-icons.css";
+
 import "./scss/style.scss";
 // Temp fix for reactstrap
 // import "../scss/core/_dropdown-menu-right.scss";
