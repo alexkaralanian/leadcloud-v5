8156c60d027b348de71638dfb0b8ed1b5b2609c6
Alex Karalanian <alex.karalanian@gmail.com>
2018-11-04 22:49:10 -0500

    dashboard refactor

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -20,6 +20,8 @@ import {
 import "./Dashboard.scss";
 
 import {
+  bar,
+  line,
   cardChartData1,
   cardChartOpts1,
   cardChartData2,
@@ -27,68 +29,180 @@ import {
 } from "./chart-data";
 
 const Dashboard = () => (
-  <Row>
-    <Col xs="12" sm="6">
-      <Card className="text-white bg-primary">
-        <CardBody className="pb-0">
-          <ButtonGroup className="float-right">
-            <ButtonDropdown
-              id="card1"
-              // isOpen={this.state.card1}
-              // toggle={() => {
-              //   this.setState({ card1: !this.state.card1 });
-              // }}
-            >
-              <DropdownToggle caret className="p-0" color="transparent">
-                <i className="icon-settings" />
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Action</DropdownItem>
-                <DropdownItem>Another action</DropdownItem>
-                <DropdownItem disabled>Disabled action</DropdownItem>
-                <DropdownItem>Something else here</DropdownItem>
-              </DropdownMenu>
-            </ButtonDropdown>
-          </ButtonGroup>
-          <h4 className="mb-0">9.823</h4>
-          <p>Members online</p>
-        </CardBody>
-        <div className="chart-wrapper px-3" style={{ height: "70px" }}>
-          <Line data={cardChartData1} options={cardChartOpts1} height={70} />
-        </div>
-      </Card>
-    </Col>
+  <React.Fragment>
+    <Row className="margin-top-2">
+      <Col xs="12" sm="6" md="3">
+        <Card className="text-white bg-primary">
+          <CardBody className="pb-0">
+            <ButtonGroup className="float-right">
+              <ButtonDropdown
+                id="card1"
+                // isOpen={this.state.card1}
+                // toggle={() => {
+                //   this.setState({ card1: !this.state.card1 });
+                // }}
+              >
+                <DropdownToggle caret className="p-0" color="transparent">
+                  <i className="icon-settings" />
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Action</DropdownItem>
+                  <DropdownItem>Another action</DropdownItem>
+                  <DropdownItem disabled>Disabled action</DropdownItem>
+                  <DropdownItem>Something else here</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+            </ButtonGroup>
+            <h4 className="mb-0">9.823</h4>
+            <p>Contacts</p>
+          </CardBody>
+          <div className="chart-wrapper px-3" style={{ height: "70px" }}>
+            <Line data={cardChartData1} options={cardChartOpts1} height={70} />
+          </div>
+        </Card>
+      </Col>
 
-    <Col xs="12" sm="6">
-      <Card className="text-white bg-info">
-        <CardBody className="pb-0">
-          <ButtonGroup className="float-right">
-            <Dropdown
-              id="card2"
-              // isOpen={this.state.card2}
-              // toggle={() => {
-              //   this.setState({ card2: !this.state.card2 });
-              // }}
-            >
-              <DropdownToggle className="p-0" color="transparent">
-                <i className="icon-location-pin" />
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Action</DropdownItem>
-                <DropdownItem>Another action</DropdownItem>
-                <DropdownItem>Something else here</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </ButtonGroup>
-          <h4 className="mb-0">9.823</h4>
-          <p>Members online</p>
-        </CardBody>
-        <div className="chart-wrapper px-3" style={{ height: "70px" }}>
-          <Line data={cardChartData2} options={cardChartOpts2} height={70} />
-        </div>
-      </Card>
-    </Col>
-  </Row>
+      <Col xs="12" sm="6" md="3">
+        <Card className="text-white bg-primary">
+          <CardBody className="pb-0">
+            <ButtonGroup className="float-right">
+              <ButtonDropdown
+                id="card1"
+                // isOpen={this.state.card1}
+                // toggle={() => {
+                //   this.setState({ card1: !this.state.card1 });
+                // }}
+              >
+                <DropdownToggle caret className="p-0" color="transparent">
+                  <i className="icon-settings" />
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Action</DropdownItem>
+                  <DropdownItem>Another action</DropdownItem>
+                  <DropdownItem disabled>Disabled action</DropdownItem>
+                  <DropdownItem>Something else here</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+            </ButtonGroup>
+            <h4 className="mb-0">9.823</h4>
+            <p>Listings</p>
+          </CardBody>
+          <div className="chart-wrapper px-3" style={{ height: "70px" }}>
+            <Line data={cardChartData1} options={cardChartOpts1} height={70} />
+          </div>
+        </Card>
+      </Col>
+
+      <Col xs="12" sm="6" md="3">
+        <Card className="text-white bg-primary">
+          <CardBody className="pb-0">
+            <ButtonGroup className="float-right">
+              <ButtonDropdown
+                id="card1"
+                // isOpen={this.state.card1}
+                // toggle={() => {
+                //   this.setState({ card1: !this.state.card1 });
+                // }}
+              >
+                <DropdownToggle caret className="p-0" color="transparent">
+                  <i className="icon-settings" />
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Action</DropdownItem>
+                  <DropdownItem>Another action</DropdownItem>
+                  <DropdownItem disabled>Disabled action</DropdownItem>
+                  <DropdownItem>Something else here</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+            </ButtonGroup>
+            <h4 className="mb-0">9.823</h4>
+            <p>Emails</p>
+          </CardBody>
+          <div className="chart-wrapper px-3" style={{ height: "70px" }}>
+            <Line data={cardChartData1} options={cardChartOpts1} height={70} />
+          </div>
+        </Card>
+      </Col>
+
+      <Col xs="12" sm="6" md="3">
+        <Card className="text-white bg-primary">
+          <CardBody className="pb-0">
+            <ButtonGroup className="float-right">
+              <ButtonDropdown
+                id="card1"
+                // isOpen={this.state.card1}
+                // toggle={() => {
+                //   this.setState({ card1: !this.state.card1 });
+                // }}
+              >
+                <DropdownToggle caret className="p-0" color="transparent">
+                  <i className="icon-settings" />
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Action</DropdownItem>
+                  <DropdownItem>Another action</DropdownItem>
+                  <DropdownItem disabled>Disabled action</DropdownItem>
+                  <DropdownItem>Something else here</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+            </ButtonGroup>
+            <h4 className="mb-0">9.823</h4>
+            <p>Campaigns</p>
+          </CardBody>
+          <div className="chart-wrapper px-3" style={{ height: "70px" }}>
+            <Line data={cardChartData1} options={cardChartOpts1} height={70} />
+          </div>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs="12" sm="12">
+        <Card>
+          <CardHeader>
+            Gross Closed Income
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Line
+                data={line}
+                options={{
+                  maintainAspectRatio: false
+                }}
+              />
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+
+      <Col xs="12" sm="12">
+        <Card>
+          <CardHeader>
+            Total Closed Units
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Bar
+                data={bar}
+                options={{
+                  maintainAspectRatio: false
+                }}
+              />
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+  </React.Fragment>
 );
 
 export default Dashboard;
--- client/src/containers/DashboardContainer/DashboardContainer.jsx
+++ client/src/containers/DashboardContainer/DashboardContainer.jsx
@@ -4,6 +4,7 @@ import { Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import Dashboard from "../../components/Dashboard/Dashboard";
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
+import Header from "../../components/Header/Header-old";
 
 class DashboardContainer extends React.Component {
   render() {
@@ -13,6 +14,15 @@ class DashboardContainer extends React.Component {
     ) : (
       <React.Fragment>
         <BreadCrumbs />
+         <Header
+          isVisible={true}
+          componentName="dashboard"
+          headerTitle="Dashboard"
+          isNew={null}
+          // primaryText="Create New"
+          // primaryFunc={() => push("/contacts/new")}
+          // primaryGlyph="plus"
+        />
         <Dashboard />
       </React.Fragment>
     );
