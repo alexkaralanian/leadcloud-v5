1ec0960794ac63e844fe5500f8143351184aece0
Alex Karalanian <alex.karalanian@gmail.com>
2019-01-06 15:44:26 -0500

    more pr cleanup

--- client/src/reducers/campaign-reducer.js
+++ client/src/reducers/campaign-reducer.js
@@ -3,15 +3,10 @@ import * as types from "../types";
 const initialState = {
   campaign: {},
   campaigns: []
-  // campaignGroups: [],
-  // campaignListings: [],
-  // listingData: []
 };
 
 const campaignReducer = (state = initialState, action) => {
   switch (action.type) {
-    // CAMPAIGN LISTINGS
-
     case types.SET_CAMPAIGN:
       return {
         ...state,
@@ -23,26 +18,6 @@ const campaignReducer = (state = initialState, action) => {
         ...state,
         campaigns: action.payload
       };
-
-    // case types.SET_CAMPAIGN_LISTINGS:
-    //   return {
-    //     ...state,
-    //     campaignListings: action.payload
-    //   };
-
-    // case types.SET_CAMPAIGN_GROUPS:
-    //   return {
-    //     ...state,
-    //     campaignGroups: action.payload
-    //   };
-
-    // case types.LOAD_LISTING_DATA: {
-    //   return {
-    //     ...state,
-    //     listingData: action.payload
-    //   };
-    // }
-
     default:
       return state;
   }
